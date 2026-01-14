2e92357594abcb1144d59b75bb2d7f36fd51bb44
tct123 <42028373+tct123@users.noreply.github.com>
2026-01-14 02:35:59 +0100

    Enable edge-to-edge and adjust soft input mode
    Added enableEdgeToEdge() in MainActivity to support edge-to-edge layouts. Updated AndroidManifest.xml to set android:windowSoftInputMode="adjustResize" for MainActivity, improving keyboard handling.

--- app/src/main/java/com/junus/mytodo/MainActivity.kt
+++ app/src/main/java/com/junus/mytodo/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.junus.mytodo.databinding.ActivityMainBinding
@@ -23,8 +24,8 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
